4bcee1a846b88516923940d07a101be89554b28c
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-07-31 17:18:09 -0700

    fix coins and kill brick

--- World/coin_area.gd
+++ World/coin_area.gd
@@ -3,7 +3,7 @@ extends Area2D
 const sound = preload("res://assets/PlatformerPack/Sounds/sfx_coin.ogg")
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerNode and not body.is_clone:
+	if body is PlayerNode and body.body_type == PlayerNode.BodyState.REAL:
 		SoundService.play_stream(sound)
 		
 		get_node("/root/Main/CurrentLevel").call_deferred("next_level")
--- World/kill_brick.gd
+++ World/kill_brick.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerNode and not body.is_clone:
+	if body is PlayerNode and body.body_type == PlayerNode.BodyState.REAL:
 		body.call_deferred("kill")
